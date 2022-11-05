2a765157093d3c1cd4e959a123ec014270ebcffc
alkomity <197023.it.eng@student.aden-univ.net>
2022-11-05 16:01:22 +0300

    finish first task

--- app/src/main/java/com/android/myfirstappincomopose/MainActivity.kt
+++ app/src/main/java/com/android/myfirstappincomopose/MainActivity.kt
@@ -3,20 +3,139 @@ package com.android.myfirstappincomopose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.android.myfirstappincomopose.ui.theme.MyFirstAppInComoposeTheme
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
+    // AppCompactActivity extends ComponentActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContent that renders the UI
+        //instead of setContentView
+        //setContent is a function that takes composable function as a parameter
         setContent { // It is a composable function
-            ErrorMessage("This is an error message")
+
+        }
+    }
+}
+
+
+/* Compose Building Blocks
+*
+* */
+//@Preview(showBackground = true)
+@Composable
+fun MyText(text: String) {
+    Text(text = text, textAlign = TextAlign.Start,
+        // I can use all the properties of Text without style
+        style = TextStyle(fontSize = 20.sp,
+            color = Red,
+            fontStyle = FontStyle.Italic,
+            fontFamily = FontFamily.SansSerif,
+            textAlign = TextAlign.Center),
+        maxLines = 2
+    )
+}
+
+@Composable
+fun MyTitle() {
+    Text(text = "Aziz",
+        style = TextStyle(fontSize = 30.sp,
+            color = Red,
+            fontStyle = FontStyle.Italic,
+            fontFamily = FontFamily.SansSerif,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            ),
+        maxLines = 2,
+    )
+}
+
+//@Preview("MyText", showBackground = true, showSystemUi = true,
+//backgroundColor = 0xFF00FF00)
+@Composable
+fun MyButton() {
+    //we should make values observable to be able to change ui
+    //remember that allow compiler observe mutableStateOf
+    // we use by to access value without using .value
+    var isClicked by remember { mutableStateOf(false) }
+    Button(onClick = { isClicked = false },
+        colors = ButtonDefaults.buttonColors(backgroundColor = Red,
+            contentColor = White,
+            disabledBackgroundColor = LightGray),
+        enabled = isClicked) {
+        Text(text = if (isClicked) "Click Me" else "Clicked")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyTextField() {
+    var searchText by remember { mutableStateOf("") }
+    TextField(value = searchText, onValueChange = {
+        searchText = it
+    }, label = { Text(text = searchText) })
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun MyImage() {
+    Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "This is an image")
+}
+
+
+@Composable
+fun MyColumn() {
+    Column {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChatItem() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        MyImage()
+        Column(horizontalAlignment = Alignment.Start) {
+            MyTitle()
+            MyText(text = "Where are you today? Are you good?")
+        }
+    }
+}
+//https://github.com/Felix-Kariuki/Yummy.git
+
+//life cycle of composable function
+//Recomposition happens data changes
+//A composable leave the composition when it's no longer on the screen
+@Composable
+fun ScoreSection(
+    score: Int,
+    countCallBack: () -> Unit,
+) {
+    Column {
+        Text("Score: $score")
+        Button(onClick = countCallBack) {
+            Text("Increase Score")
         }
     }
 }
@@ -25,7 +144,7 @@ class MainActivity : ComponentActivity() {
 //Public : Make your composable functions public
 //To make compiler know that function will render ui on screen
 @Composable //To use this function in the UI, we need to annotate it with @Composable
-fun ErrorMessage(error: String) { //return type is not required
+fun ErrorMessage(error: String = "Error") { //return type is not required
     Text(text = error)
 }
 
